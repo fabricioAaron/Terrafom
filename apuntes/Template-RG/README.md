971d55f80f098964c65a403168052435dd210213
fabricioAaron <107084766+fabricioAaron@users.noreply.github.com>
2026-03-21 11:46:16 +0100

    actualización

--- apuntes/Template-RG/README.md
+++ apuntes/Template-RG/README.md
@@ -4,3 +4,5 @@
 <img width="997" height="636" alt="imagen" src="https://github.com/user-attachments/assets/e59d1637-64cd-446a-8ffd-1beed96842e9" />
 
 ~> : significa que se mantendría la version : 3.0.x 
+
+1. Previamente tenemos que cargar un msi
---
+++ apuntes/Template-RG/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers { #aquí se definen los proveedores que se van a utilizar, en este caso el proveedor de Azure
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 4.65.0"
+        }
+  }
+  required_version = ">=1.14.0" #aquí se define la versión mínima de terraform que se requiere para ejecutar este código
+}
+
+provider "azurerm" { #aquí se configura el proveedor de Azure, en este caso se habilita la característica de "features" que es necesaria para utilizar algunos recursos de Azure
+    features {
+      
+    }
+  
+}
+
+
+
+#sería el tipo que recurso que quiero aplicar 
+resource "azurerm_resource_group" "example" { #lo que esta entre comidas es el nombre del recurso, que es  interno para terraform, no tiene que ser igual al de azure 
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+
+  name                     = "template-001"
+  resource_group_name      = azurerm_resource_group.example.name #aquí se hace referencia al recurso anterior, para que tome el nombre del grupo de recursos creado anteriormente
+  location                 = azurerm_resource_group.example.location #sería una dependencia, porque el recurso de la cuenta de almacenamiento depende del grupo de recursos, entonces se hace referencia a la ubicación del grupo de recursos creado anteriormente
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}

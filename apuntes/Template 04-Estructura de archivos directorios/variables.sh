211c16e77ddf30afed862ad85eb5d0c155a6f7a8
fabricioAaron <107084766+fabricioAaron@users.noreply.github.com>
2026-03-23 17:59:32 +0100

    estructura de archivos Terraform

--- apuntes/Template 04-Estructura de archivos directorios/variables.sh
+++
@@ -1,9 +0,0 @@
-  "appId": "3fb92e8b-76b6-4c6c-9342-a312980c2203",
-  "displayName": "az-demo",
-  "password": "xhh8Q~1Xl8q3gD9nWT4svRarYOdVLRBSh3Nggca8",
-  "tenant": "f6199e1a-0914-4569-840b-3fae0d4ac37a"
-
-  export ARM_CLIENT_ID="3fb92e8b-76b6-4c6c-9342-a312980c2203"
-export ARM_CLIENT_SECRET="xhh8Q~1Xl8q3gD9nWT4svRarYOdVLRBSh3Nggca8"
-export ARM_SUBSCRIPTION_ID="2f63422b-aa39-4849-85c0-805265f3db8f"
-export ARM_TENANT_ID="f6199e1a-0914-4569-840b-3fae0d4ac37a"
